0248c784f51f585c5a9caee8d749f96e63a0647f
bogdanost <bo12@kent.ac.uk>
2026-03-03 13:31:58 +0000

    project planning

---
+++ project.py
@@ -0,0 +1,24 @@
+import random
+class passwords:
+    def __init__(self):
+        pass
+
+
+
+    def generate():
+        #use random and whatever
+
+    
+
+
+    def save_on_pc():
+        #use I/O
+
+
+    def check_validaty():
+        #use regular expressions
+
+
+class login:
+    pass
+#same in here
